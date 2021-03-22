f41e60883f0e2d76cf1312eb1e987ad0191e78dd
tikke tikke <tikke.flex@yandex.ru>
2021-03-22 11:12:04 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace laba5_IT
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      int index = listBox1.SelectedIndex;
+      string str = (string)listBox1.Items[index];
+      var strWithoutSpaces = str.Replace(" ", "");
+      label1.Text = strWithoutSpaces;
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
